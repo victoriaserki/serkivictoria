d4340f69c78354718f96327c70a72cc339d2b9b6
victoriaserki <serkivictoria@gmail.com>
2025-08-25 09:27:35 -0300

    algoritos clase 03

---
+++ escritorio01/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace escritorio01
+{
+    partial class ejercicio01
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            lblsalida = new Label();
+            label5 = new Label();
+            btnAceptar = new Button();
+            btnsalir = new Button();
+            txtnombre = new TextBox();
+            txtApellido = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 0;
+            label1.Text = "ejercicio01";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 51);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre:";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 95);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Apellido:";
+            // 
+            // lblsalida
+            // 
+            lblsalida.AutoSize = true;
+            lblsalida.Location = new Point(151, 270);
+            lblsalida.Name = "lblsalida";
+            lblsalida.Size = new Size(0, 15);
+            lblsalida.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(119, 185);
+            label5.Name = "label5";
+            label5.Size = new Size(32, 15);
+            label5.TabIndex = 4;
+            label5.Text = "label";
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Location = new Point(50, 140);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(75, 23);
+            btnAceptar.TabIndex = 5;
+            btnAceptar.Text = "Aceptar";
+            btnAceptar.UseVisualStyleBackColor = true;
+            btnAceptar.Click += btnAceptar_Click;
+            // 
+            // btnsalir
+            // 
+            btnsalir.Location = new Point(151, 140);
+            btnsalir.Name = "btnsalir";
+            btnsalir.Size = new Size(75, 23);
+            btnsalir.TabIndex = 6;
+            btnsalir.Text = "Salir";
+            btnsalir.UseVisualStyleBackColor = true;
+            btnsalir.Click += btnsalir_Click;
+            // 
+            // txtnombre
+            // 
+            txtnombre.Location = new Point(89, 48);
+            txtnombre.Name = "txtnombre";
+            txtnombre.Size = new Size(100, 23);
+            txtnombre.TabIndex = 7;
+            txtnombre.TextChanged += textBox1_TextChanged;
+            // 
+            // txtApellido
+            // 
+            txtApellido.Location = new Point(89, 92);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(100, 23);
+            txtApellido.TabIndex = 8;
+            txtApellido.TextChanged += txtApellido_TextChanged;
+            // 
+            // ejercicio01
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(958, 450);
+            Controls.Add(txtApellido);
+            Controls.Add(txtnombre);
+            Controls.Add(btnsalir);
+            Controls.Add(btnAceptar);
+            Controls.Add(label5);
+            Controls.Add(lblsalida);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "ejercicio01";
+            Text = "escritorio";
+            TopMost = true;
+            Load += lblTituloejercicio_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label lblsalida;
+        private Label label5;
+        private Button btnAceptar;
+        private Button btnsalir;
+        private TextBox txtnombre;
+        private TextBox txtApellido;
+    }
+}
---
+++ escritorio01/Form1.cs
@@ -0,0 +1,50 @@
+namespace escritorio01
+{
+    public partial class ejercicio01 : Form
+    {
+        public ejercicio01()
+        {
+            InitializeComponent();
+        }
+
+        private void lblTituloejercicio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            lblsalida.Text = txtnombre.Text + " " + txtApellido.Text;
+        }
+
+        private void txtApellido_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
